b26bbfdad0dfa21b20b1b44ff8e0bfa31470dc80
Nicholas Ulricksen <n.ulricksen@gmail.com>
2020-11-15 04:00:32 -0800

    file extension lookup

---
+++ util/fileExtensions.go
@@ -0,0 +1,19 @@
+package util
+
+var FileExt = map[string]string{
+	"swift":      "swift",
+	"golang":     "go",
+	"python":     "py",
+	"python3":    "py",
+	"c":          "c",
+	"csharp":     "cs",
+	"javascript": "js",
+	"java":       "java",
+	"ruby":       "rb",
+	"scala":      "scala",
+	"rust":       "rs",
+	"typescript": "ts",
+	"kotlin":     "kotlin",
+	"cpp":        "cpp",
+	"php":        "php",
+}
